f73fe92dcefc0738ca8ed919b86ce985e3aed90a
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-17 19:42:36 +0100

    lexxer done, workin progress

--- shell.py
+++ shell.py
@@ -1,3 +1,8 @@
+import dao
+
 while True:
     text = input('My-Own-Langage > ')
-    print(text)
+    result, error = dao.run('<stdin>', text)
+
+    if error: print(error.as_string())
+    else: print(result)
