3f2c81be550aa0efdf87bd39ea357a371cd6cf09
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-19 18:00:41 +0100

    defintions of operations

--- dao.py
+++ dao.py
@@ -138,7 +138,52 @@ class Lexer:
             return Token(TT_INT, int(num_str))
         else:
             return Token(TT_FLOAT, float(num_str))
+        
+#######################################
+# NODES
+#######################################
+        
+class NumberNode:
+    def __init__(self, tok):
+        self.tok = tok
+    
+    def __repr__(self):
+        return f'{self.tok}'
+    
+class BinOpNode:
+    def __init__(self, left_node, op_tok, right_node):
+        self.left_node = left_node
+        self.op_tok = op_tok
+        self.right_node = right_node
+    
+    def __repr__(self):
+        return f'({self.left_node}, {self.op_tok}, {self.right_node})'
+    
+#######################################
+# PARSER
+#######################################
 
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.tok_idx = 1
+        self.advance()
+
+    def advance(self):
+        self.tok_idx += 1
+        if self.tok_idx < len(self.tokens):
+            self.current_tok = self.tokens[self.tok_idx]
+        return self.current_tok
+    
+    def factor(self):
+        tok = self.current_tok
+
+        if tok.type in (TT_INT, TT_FLOAT):
+            self.advance()
+            return NumberNode(tok)
+    
+    def term(self):
+        return self.bin_op(self.factor, (TT_MUL, TT_DIV))
 #######################################
 # RUN
 #######################################
